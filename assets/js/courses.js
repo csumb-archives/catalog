c70ff9abe37bf2588dfb8a46de2bef7bd8cb9e7e
Kevin Miller <kevin@csumb.edu>
2016-03-17 11:45:36 -0700

    Auto linked courses

--- assets/js/courses.js
+++ assets/js/courses.js
@@ -4,7 +4,7 @@ define(function(require) {
 
   return {
 
-    regex : /[A-Z]{2,} [0-9]{2,}(\/?)([A-Z]?)/,
+    regex : /[A-Z]{2,} [0-9]{2,}(\/?)([A-Z]?)/g,
 
     term : '',
 
@@ -16,13 +16,6 @@ define(function(require) {
       var that = this;
       this.term = $('body').data('term');
       this.prepareModal();
-      this.fixNewCatalogLinks();
-      /*$('.catalog-archive-content a:not(.catalog-tip)').each(function() {
-        that.removeCourseLink($(this));
-      });
-      $('.catalog-archive-content *').each(function() {
-
-      });*/
       this.bindLinks();
     },
 
@@ -32,24 +25,12 @@ define(function(require) {
       $('body').append('<div id="temp-course-modal">');
     },
 
-    fixNewCatalogLinks : function() {
-      this.$scope.find('a.catalog-tip').each(function() {
-        if($(this).attr('id')) {
-          var $newLink = $('<a>');
-          $newLink.attr('href', '/courses/json/' + $(this).attr('id').toLowerCase() + '.json')
-                 .addClass('course-link')
-                 .html($(this).text());
-          $(this).replaceWith($newLink);
-        }
-      });
-    },
-
     bindLinks : function() {
       var that = this;
       this.$scope.find('a.course-link').on('click', function(event) {
         event.preventDefault();
         $.getJSON($(this).attr('href'), function(data) {
-          var html = that.modalTemplate(data[2164]);
+          var html = that.modalTemplate(data[that.term]);
           $('#temp-course-modal').html(html);
           $('#temp-course-modal .modal').modal('show');
         });
